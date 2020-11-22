b9b3c9c486731404e9a7e39d50f630840f772bc9
elbaro <elbaro@users.noreply.github.com>
2020-11-23 00:07:08 +0900

    commit

--- examples/button.rs
+++ examples/button.rs
@@ -8,6 +8,7 @@ struct App {
     button4: iced::button::State,
     button5: iced::button::State,
     button6: iced::button::State,
+    text_input: iced::text_input::State,
 }
 
 #[derive(Clone, Debug)]
@@ -27,15 +28,44 @@ impl iced::Sandbox for App {
     fn view(&mut self) -> iced::Element<Self::Message> {
         let col = iced::Column::new()
             .push(iced_milligram_theme::widget::h1("Getting Started"))
-            .push(iced_milligram_theme::widget::paragraph_text("There are some ways to get started. First, see all download options available below, then choose the most suitable option for your need. Now you should add the main file of the Milligram and the CSS Reset in the header of your project. Just that!"))
+            .push(iced_milligram_theme::widget::paragraph_text(
+                "This is a port of ",
+            ))
             .push(iced_milligram_theme::widget::h1("Buttons"))
-            .push(iced_milligram_theme::widget::paragraph(iced_milligram_theme::widget::large_button(&mut self.button1, "large button").on_press(Message::ButtonPressed)))
-            .push(iced_milligram_theme::widget::paragraph(iced_milligram_theme::widget::large_outlined_button(&mut self.button2, "large outlined button").on_press(Message::ButtonPressed)))
-            .push(iced_milligram_theme::widget::paragraph(iced_milligram_theme::widget::large_clear_button(&mut self.button3, "large cleard button").on_press(Message::ButtonPressed)))
-            .push(iced_milligram_theme::widget::paragraph(iced_milligram_theme::widget::button(&mut self.button4, "normal default button").on_press(Message::ButtonPressed)))
+            .push(iced_milligram_theme::widget::paragraph(
+                iced_milligram_theme::widget::large_button(&mut self.button1, "large button")
+                    .on_press(Message::ButtonPressed),
+            ))
+            .push(iced_milligram_theme::widget::paragraph(
+                iced_milligram_theme::widget::large_outlined_button(
+                    &mut self.button2,
+                    "large outlined button",
+                )
+                .on_press(Message::ButtonPressed),
+            ))
+            .push(iced_milligram_theme::widget::paragraph(
+                iced_milligram_theme::widget::large_clear_button(
+                    &mut self.button3,
+                    "large cleard button",
+                )
+                .on_press(Message::ButtonPressed),
+            ))
+            .push(iced_milligram_theme::widget::paragraph(
+                iced_milligram_theme::widget::button(&mut self.button4, "normal default button")
+                    .on_press(Message::ButtonPressed),
+            ))
             .push(iced_milligram_theme::widget::h1("Text List"))
-            .push(iced_milligram_theme::widget::unordered_list(&["Apple", "Banana", "Orange"]))
-            ;
+            .push(iced_milligram_theme::widget::unordered_list(&[
+                "Apple", "Banana", "Orange",
+            ]))
+            .push(
+                iced_milligram_theme::widget::text_input::<Self::Message, _>(
+                    &mut self.text_input,
+                    "placeholder",
+                    "value",
+                    |_s| Message::ButtonPressed,
+                ),
+            );
 
         iced::Container::new(iced::Container::new(col).width(iced::Length::Units(800)))
             .width(iced::Length::Fill)
--- src/widget/form.rs
+++ src/widget/form.rs
@@ -1,3 +1,13 @@
-// pub fn text_input() {
-
-// }
+pub fn text_input<'a, Message, F>(
+    state: &'a mut iced::text_input::State,
+    placeholder: &'a str,
+    value: &'a str,
+    on_change: F,
+) -> iced::TextInput<'a, Message>
+where
+    F: 'static + Fn(String) -> Message,
+{
+    iced::TextInput::new(state, placeholder, value, on_change)
+        .padding(10)
+        .size(16)
+}
